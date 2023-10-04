5c068d296a304ad9a77a3c5ef2dba7b5e99b8bd6
cdragonzfr <146982306+cdragonzfr@users.noreply.github.com>
2023-10-04 12:57:45 -0400

    Create file_migration.ps1

---
+++ file_migration.ps1
@@ -0,0 +1,66 @@
+param(
+    [Parameter(Mandatory=$true)]
+    [string]$DriveLetter,
+    [Parameter(Mandatory=$true)]
+    [string]$NetworkPath,
+    [Parameter(Mandatory=$true)]
+    [string]$DirectoriesCsv,
+    [Parameter(Mandatory=$true)]
+    [string]$FilesCsv,
+    [Parameter(Mandatory=$true)]
+    [string]$OwnerUser,
+    [Parameter(Mandatory=$true)]
+    [string]$LogFile
+)
+
+# Map Network Drive
+net use $DriveLetter $NetworkPath
+
+# Create Directories and set ACL
+Import-Csv $DirectoriesCsv | ForEach-Object {
+    $directory = $_.DirectoryPath
+
+    # Create directory
+    New-Item -Path $directory -ItemType Directory
+
+    # Set ACL
+    $acl = Get-Acl -Path $directory
+    $everyone = New-Object system.security.principal.NTAccount("Everyone")
+    $administrators = New-Object system.security.principal.NTAccount("Administrators")
+    $owner = New-Object system.security.principal.NTAccount($OwnerUser)
+
+    $everyonePermission = New-Object system.security.accesscontrol.filesystemaccessrule($everyone, "FullControl", "Allow")
+    $adminPermission = New-Object system.security.accesscontrol.filesystemaccessrule($administrators, "FullControl", "Allow")
+
+    $acl.SetAccessRuleProtection($true, $false)
+    $acl.SetOwner($owner)
+    $acl.AddAccessRule($everyonePermission)
+    $acl.AddAccessRule($adminPermission)
+    Set-Acl -Path $directory -AclObject $acl
+
+    Add-Content -Path $LogFile -Value "Directory $directory created and permissions set."
+}
+
+# Move Files and set ACL
+Import-Csv $FilesCsv | ForEach-Object {
+    $source = $_.SourcePath
+    $destination = $_.DestinationPath
+
+    # Move file
+    Move-Item -Path $source -Destination $destination
+
+    # Set ACL
+    $acl = Get-Acl -Path $destination
+    $everyone = New-Object system.security.principal.NTAccount("Everyone")
+    $administrators = New-Object system.security.principal.NTAccount("Administrators")
+
+    $everyonePermission = New-Object system.security.accesscontrol.filesystemaccessrule($everyone, "FullControl", "Allow")
+    $adminPermission = New-Object system.security.accesscontrol.filesystemaccessrule($administrators, "FullControl", "Allow")
+
+    $acl.SetAccessRuleProtection($true, $false)
+    $acl.AddAccessRule($everyonePermission)
+    $acl.AddAccessRule($adminPermission)
+    Set-Acl -Path $destination -AclObject $acl
+
+    Add-Content -Path $LogFile -Value "File $source moved to $destination and permissions set."
+}
